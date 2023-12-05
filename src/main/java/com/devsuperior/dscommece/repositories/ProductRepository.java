34c6a405df865aacc1a9511c1b639f0324fdefcc
ElderBR <elderbarbosaromao@gmail.com>
2023-12-05 18:44:04 -0300

    Criado a Repository do Product

--- src/main/java/com/devsuperior/dscommece/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommece/repositories/ProductRepository.java
@@ -1,2 +1,9 @@
-package com.devsuperior.dscommece.repositories;public interface ProductRepository {
+package com.devsuperior.dscommece.repositories;
+
+import com.devsuperior.dscommece.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
