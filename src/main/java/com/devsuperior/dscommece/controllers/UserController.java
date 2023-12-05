d7a43233ce7c80af2f4d9d8d653c7ac586ea0c75
ElderBR <elderbarbosaromao@gmail.com>
2023-12-04 23:27:08 -0300

    Criando o controller com o método findAll e dto da classe User

--- src/main/java/com/devsuperior/dscommece/controllers/UserController.java
+++ src/main/java/com/devsuperior/dscommece/controllers/UserController.java
@@ -1,2 +1,26 @@
-package com.devsuperior.dscommece.controllers;public class UserController {
+package com.devsuperior.dscommece.controllers;
+
+import com.devsuperior.dscommece.dto.UserDTO;
+import com.devsuperior.dscommece.entities.User;
+import com.devsuperior.dscommece.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<UserDTO> findAll(){
+        return service.findAll();
+    }
+
 }
--- src/main/java/com/devsuperior/dscommece/dto/UserDTO.java
+++ src/main/java/com/devsuperior/dscommece/dto/UserDTO.java
@@ -1,2 +1,45 @@
-package com.devsuperior.dscommece.dto;public class UserDTO {
+package com.devsuperior.dscommece.dto;
+
+import com.devsuperior.dscommece.entities.User;
+
+import java.time.LocalDate;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+        phone = entity.getPhone();
+        birthDate = entity.getBirthDate();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
 }
