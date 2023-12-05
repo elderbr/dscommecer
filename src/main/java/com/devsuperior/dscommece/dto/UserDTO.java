474dc76180ebaae38695289cd8048aa0b034d4aa
ElderBR <elderbarbosaromao@gmail.com>
2023-12-05 17:41:42 -0300

    Importando e adicionando validação no UserDTO

--- src/main/java/com/devsuperior/dscommece/dto/UserDTO.java
+++ src/main/java/com/devsuperior/dscommece/dto/UserDTO.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommece.dto;
 
 import com.devsuperior.dscommece.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -27,18 +31,25 @@ public class UserDTO {
         return id;
     }
 
+    @NotBlank(message = "O nome não pode ficar em branco!!!")
     public String getName() {
         return name;
     }
 
+
+    @Email(message = "Digite o email corretamente!!!")
     public String getEmail() {
         return email;
     }
 
+    @NotBlank
     public String getPhone() {
         return phone;
     }
 
+    @NotBlank(message = "Digite a data de nascimento!!!")
+    @Past(message = "A data de nascimento deve estar no passado")
+    @DateTimeFormat(pattern = "yyyy-MM-dd", iso = DateTimeFormat.ISO.DATE)
     public LocalDate getBirthDate() {
         return birthDate;
     }
