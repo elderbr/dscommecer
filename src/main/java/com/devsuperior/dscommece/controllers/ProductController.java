5982d22bf5d1bf86017b0361a342ebc3d484c6cf
ElderBR <elderbarbosaromao@gmail.com>
2023-12-05 18:44:51 -0300

    Criando o método para lista todos os produtos e buscar pelo ID

--- src/main/java/com/devsuperior/dscommece/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommece/controllers/ProductController.java
@@ -1,2 +1,30 @@
-package com.devsuperior.dscommece.controllers;public class ProductController {
+package com.devsuperior.dscommece.controllers;
+
+import com.devsuperior.dscommece.dto.ProductDTO;
+import com.devsuperior.dscommece.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findByID(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
 }
--- src/main/java/com/devsuperior/dscommece/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommece/services/ProductService.java
@@ -1,2 +1,34 @@
-package com.devsuperior.dscommece.services;public class ProductService {
+package com.devsuperior.dscommece.services;
+
+import com.devsuperior.dscommece.dto.ProductDTO;
+import com.devsuperior.dscommece.entities.Product;
+import com.devsuperior.dscommece.repositories.ProductRepository;
+import com.devsuperior.dscommece.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> page = repository.findAll(pageable);
+        return page.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        try {
+            Product product = repository.findById(id).get();
+            return new ProductDTO(product);
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("O produto não existe!!!");
+        }
+    }
 }
