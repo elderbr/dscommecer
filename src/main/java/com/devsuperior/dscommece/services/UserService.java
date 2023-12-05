635e1db5fc8b890b341febf72e09c28331dade5c
ElderBR <elderbarbosaromao@gmail.com>
2023-12-05 17:09:03 -0300

    Refaturado o método para buscar pelo ID, refaturando o método findAll retornando Page

--- src/main/java/com/devsuperior/dscommece/services/UserService.java
+++ src/main/java/com/devsuperior/dscommece/services/UserService.java
@@ -3,7 +3,10 @@ package com.devsuperior.dscommece.services;
 import com.devsuperior.dscommece.dto.UserDTO;
 import com.devsuperior.dscommece.entities.User;
 import com.devsuperior.dscommece.repositories.UserRepository;
+import com.devsuperior.dscommece.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,14 +18,18 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<UserDTO> findAll(){
-        List<User> users = repository.findAll();
-        return users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    public Page<UserDTO> findAll(Pageable pageable){
+        Page<User> pages = repository.findAll(pageable);
+        return pages.map(x->new UserDTO(x));
     }
 
     public UserDTO findById(Long id){
-        User user = repository.findById(id).get();
-        return new UserDTO(user);
+        try {
+            User user = repository.findById(id).get();
+            return new UserDTO(user);
+        }catch (Exception e){
+            throw new ResourceNotFoundException("Usuário não existe!!!");
+        }
     }
 
 }
