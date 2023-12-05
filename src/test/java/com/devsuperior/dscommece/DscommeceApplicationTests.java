2a6ebfae51185f6796777b2f7208ce86ecccb286
ElderBR <elderbarbosaromao@gmail.com>
2023-12-04 21:17:05 -0300

    First commit

---
+++ src/test/java/com/devsuperior/dscommece/DscommeceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
