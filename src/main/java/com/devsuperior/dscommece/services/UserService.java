1b2e2e26b9e1e404797f805c76e63a5b8912331e
ElderBR <elderbarbosaromao@gmail.com>
2023-12-04 23:26:36 -0300

    Criando o Service da entidade User

--- src/main/java/com/devsuperior/dscommece/services/UserService.java
+++ src/main/java/com/devsuperior/dscommece/services/UserService.java
@@ -1,2 +1,23 @@
-package com.devsuperior.dscommece.services;public class UserService {
+package com.devsuperior.dscommece.services;
+
+import com.devsuperior.dscommece.dto.UserDTO;
+import com.devsuperior.dscommece.entities.User;
+import com.devsuperior.dscommece.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<UserDTO> findAll(){
+        List<User> users = repository.findAll();
+        return users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    }
+
 }
